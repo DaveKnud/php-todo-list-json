02944f4f39ed4f1ee386551a41f9f56330c29133
DaveKnud <daveknud@outlook.com>
2023-06-23 01:01:38 +0200

    stampati dati php tramite vue

--- tmp/index.php
+++ tmp/index.php
@@ -1,15 +1,18 @@
 
 
     <?php
+    header("Access-Control-Allow-Origin: http://localhost:5173");
+    header("Access-Control-Allow-Headers: X-Requested-With");
+    
     header('Content-Type: application/json');
 
-    $toDoList = [
-        ['language' => 'HTML'],
-        ['language' => 'CSS'],
-        ['language' => 'Responsive Design'],
-        ['language' => 'Javascript'],
-        ['language' => 'PHP'],
-        ['language' => 'Lavarel'],
+    $programmingLanguages  = [
+        ['name' => 'HTML'],
+        ['name' => 'CSS'],
+        ['name' => 'Responsive Design'],
+        ['name' => 'Javascript'],
+        ['name' => 'PHP'],
+        ['name' => 'Lavarel'],
     ];
-        echo json_encode($toDoList);
+        echo json_encode($programmingLanguages );
     ?>
